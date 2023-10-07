93f561e239e5ad85129bd0896ea24d28247b79f0
DanielD26 <103547416@student.swin.edu.au>
2023-10-07 14:41:25 +1100

    First Commit

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import SpeechToText from "./components/speechToText/SpeechToText";
+
+const App = () => {
+  return (
+    <SpeechToText />
+  )
+
+}
+
+export default App;
---
+++ components/speechToText/SpeechToText.jsx
@@ -0,0 +1,13 @@
+// SPEECH TO TEXT FUNCTIONALITY HERE
+import {
+  Text
+} from 'react-native'
+
+
+SpeechToText = () => {
+  return ( 
+    <Text>Speech to text functionality here</Text>
+  )
+  
+}
+export default SpeechToText;
